32a6adb9f48f3bfea045fd4447aea96351f4792f
Mario <mario.gonzalez@hotmail.com.ar>
2022-09-19 14:19:13 -0300

    Declaraciones de funciones

--- Auto/Proyecto/SOFTWARE/Grupo6_Auto/src/main.cpp
+++ Auto/Proyecto/SOFTWARE/Grupo6_Auto/src/main.cpp
@@ -1,38 +1,39 @@
 #include <Arduino.h>
 
-
-
-
-
+int Dire1 =12; // pin de puente H manejo de direccion Mecanica 
+int Dire2 =11; // pin de puente H manejo de direccion Mecanica 
+int Velocidad = 10; // manjo de velocidad por PWM al puente H
+int Led= 2;
 
 void adelante(){
-
+  digitalWrite(Dire1, HIGH);
+  digitalWrite(Dire2, HIGH);
 }
 
 void atras(){
-
+  digitalWrite(Dire1, LOW);
+  digitalWrite(Dire2, LOW);
 }
 
 void izquierda(){
-
+  digitalWrite(Dire1, HIGH;
+  digitalWrite(Dire2, LOW);
 }
 
 void derecha(){
-
-  
+  digitalWrite(Dire1, LOW;
+  digitalWrite(Dire2, HIGH);
 }
-
-
 
 void setup(){
   Serial.begin(9600);
-    pinMode(2, OUTPUT);
-
+  pinMode(Led, OUTPUT);
+  pinMode(Dire1, OUTPUT); 
+  pinMode(Dire2, OUTPUT);
+  pinMode(velocidad, OUTPUT); 
 }
 
 void loop(){
-    digitalWrite(2,HIGH);
-    delay(1000);
-    digitalWrite(2,LOW);
-    delay(1000);
+
 }
+
