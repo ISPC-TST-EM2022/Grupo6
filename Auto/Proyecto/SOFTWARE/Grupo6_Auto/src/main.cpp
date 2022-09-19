878720ec63438aea739a1ca58355aec0656af176
Mario <mario.gonzalez@hotmail.com.ar>
2022-09-19 13:45:54 -0300

    Blink

--- Auto/Proyecto/SOFTWARE/Grupo6_Auto/src/main.cpp
+++ Auto/Proyecto/SOFTWARE/Grupo6_Auto/src/main.cpp
@@ -3,11 +3,15 @@
 
 
 
-void setup() {
+void setup(){
   Serial.begin(9600);
-  
+    pinMode(2, OUTPUT);
+
 }
 
-void loop() {
-  
+void loop(){
+    digitalWrite(2,HIGH);
+    delay(1000);
+    digitalWrite(2,LOW);
+    delay(1000);
 }
