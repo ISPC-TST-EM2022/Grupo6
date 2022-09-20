0c622e07b9886884ec4b48de75d9f0060ce1c9b9
Mario <mario.gonzalez@hotmail.com.ar>
2022-09-20 10:28:28 -0300

    sensor

--- Auto/Proyecto/SOFTWARE/Grupo6_Auto/src/main.cpp
+++ Auto/Proyecto/SOFTWARE/Grupo6_Auto/src/main.cpp
@@ -2,43 +2,48 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-int Dire1 = 12;                                       // pin de puente H manejo de direccion Mecanica
-int Dire2 = 11;                                       // pin de puente H manejo de direccion Mecanica
-int Velocidad = 10;                                   // manejo de velocidad por PWM al puente H
-int Led = 2;                                          // Led indicador de comando recibido
-const char *ssid = "TP-LINK_B33E";                       // nombre de la red a donde me conecto
-const char *password = "50868155";               // contraseña de red WIFI
+int Dire1 = 12;     // pin de puente H manejo de direccion Mecanica
+int Dire2 = 11;     // pin de puente H manejo de direccion Mecanica
+int Velocidad = 10; // manejo de velocidad por PWM al puente H
+int Led = 2;
+int Sensor = 25;                                      // Led indicador de comando recibido
+const char *ssid = "TP-LINK_B33E";                    // nombre de la red a donde me conecto
+const char *password = "50868155";                    // contraseña de red WIFI
 const char *mqttServer = "mgalarmasserver1.ddns.net"; // dns del broker
 const int mqttPort = 1883;                            // puerto soket del broker
 const char *mqttUser = "";                            // usuario del boker
 const char *mqttPassword = "";                        // password del broker
 const char *topic = "/grupo6/app/";
 
-
 WiFiClient espClient;           // invoco al constructor de la clase Wificlient
 PubSubClient client(espClient); // establecemos objeto de la clase espClient
 
-void adelante(){ // declaramos funcion para ir adelante
+void adelante()
+{ // declaramos funcion para ir adelante
   digitalWrite(Dire1, HIGH);
   digitalWrite(Dire2, HIGH);
 }
 
-void atras(){ // declaramos funcion para ir para atras
+void atras()
+{ // declaramos funcion para ir para atras
   digitalWrite(Dire1, LOW);
   digitalWrite(Dire2, LOW);
 }
 
-void izquierda(){ // declaramos funcion para ir izquierda
+void izquierda()
+{ // declaramos funcion para ir izquierda
   digitalWrite(Dire1, HIGH);
   digitalWrite(Dire2, LOW);
 }
 
-void derecha(){ // declaramos funcion para ir derecha
+void derecha()
+{ // declaramos funcion para ir derecha
   digitalWrite(Dire1, LOW);
   digitalWrite(Dire2, HIGH);
 }
 
-void Suscribe_MQTT(){
+void Suscribe_MQTT()
+{
   client.subscribe(topic); // client.subscribe(ID+topico);
 }
 
@@ -50,12 +55,20 @@ void callback(char *topic, byte *payload, unsigned int length)
   {
     for (int i = 0; i < 10; i++)
     {
-          digitalWrite(Led, HIGH);
-          delay(1000);
-          digitalWrite(Led, LOW);
-          delay(1000);
+      digitalWrite(Led, HIGH);
+      delay(1000);
+      digitalWrite(Led, LOW);
+      delay(1000);
     }
-      }
+  }
+}
+
+void medir()
+{
+  int mq7 = analogRead(Sensor);                      // Lemos la salida analógica del MQ
+  float voltaje = mq7 * (5.0 / 1023.0);              // Convertimos la lectura en un valor de voltaje
+  float Rs = 1000 * ((5 - voltaje) / voltaje);       // preset con una resitencia ideal de 1000 ohms
+  double monoxido = 0.4091 * pow(Rs / 5463, -1.497); // concentración  de CO ecu segun fabricante
 }
 
 void setup()
@@ -69,7 +82,7 @@ void setup()
     delay(500);
     Serial.print(".");
   }
-   Serial.println("");
+  Serial.println("");
   Serial.println("Conexion Exitosa a la red de wifi !!"); // publicamos al salir del while
 
   client.setServer(mqttServer, mqttPort); // me conecto al broker
@@ -79,7 +92,8 @@ void setup()
     clientId += String(random(0xffff), HEX); // sumo al usuario numero random para que me acepte broker
 
     Serial.println("Conectando al broker MQTT...");
-    if (client.connect(clientId.c_str())){ // usuario broker random ante caidas
+    if (client.connect(clientId.c_str()))
+    { // usuario broker random ante caidas
       Serial.println("Conectado !!!");
       Serial.print("Usuario: ");
       Serial.print(clientId.c_str());
@@ -92,13 +106,14 @@ void setup()
     }
   }
   client.setCallback(callback);
-
   pinMode(Led, OUTPUT);       // establecemos pin como salida
   pinMode(Dire1, OUTPUT);     // establecemos pin como salida
   pinMode(Dire2, OUTPUT);     // establecemos pin como salida
   pinMode(Velocidad, OUTPUT); // establecemos pin como salida
 }
 
-void loop(){
-client.loop();
+void loop()
+{
+  client.loop();
+  
 }
