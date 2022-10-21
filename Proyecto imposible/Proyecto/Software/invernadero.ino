5440aab59a1801a6389c642e6f8133083de8b576
Mario <mario.gonzalez@hotmail.com.ar>
2022-10-21 17:30:42 -0300

    LCD

--- Proyecto imposible/Proyecto/Software/invernadero.ino
+++ Proyecto imposible/Proyecto/Software/invernadero.ino
@@ -1,33 +1,48 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <Wire.h>            // incluimos libreria de comunicacion I2C nativa
-#include <Adafruit_BMP280.h> // incluimos libreria de adafruit para sensor BMP280
-#include "DHT.h"
-#define DHTPIN 2 
+/*
+* Prototipo de Invernadero Hidroponico 
+* ISPC Telecomunicaciones Cohorte 2022
+* Grupo 6 
+* Alumnos   
+*/
 
-//const char *ssid = "TP-LINK_B33E";                     // red de wifi a la que me conecto
-//const char *password = "50868155";                     // password de la red de wifi
+
+
+#include <WiFi.h>               // incluimos libreria de manejo de wifi
+#include <PubSubClient.h>       // incluimos libreria de publicaciones y susbcripciones
+#include <Wire.h>               // incluimos libreria de comunicacion I2C nativa
+#include <Adafruit_BMP280.h>    // incluimos libreria de adafruit para sensor BMP280
+#include <DHT.h>                // incluimos libreria de DHT11
+#include <LiquidCrystal_I2C.h>  // incluimos libreria de LCD 20x4 I2C
+
+
+#define DHTPIN 26 
+#define DHTTYPE DHT11           // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+//const char *ssid = "TP-LINK_B33E";                // red de wifi a la que me conecto
+//const char *password = "50868155";                // password de la red de wifi
 const char *ssid = "MGAlarmas";                     // red de wifi a la que me conecto
-const char *password = "mgalarmas3040";                     // password de la red de wifi
+const char *password = "mgalarmas3040";             // password de la red de wifi
 const char *mqtt_server = "mgalarmasserver1.ddns.net"; // dns del server mosquitto (MQTT)
-unsigned int mqtt_port = 1883;                         // socket port del server MQTT Mosquitto
-const char *Topico = "/invernadero/grupo6/";           // topico para publicar los datos en el server
+unsigned int mqtt_port = 1883;                      // socket port del server MQTT Mosquitto
+const char *Topico = "/invernadero/grupo6/";        // topico para publicar los datos en el server
 
-Adafruit_BMP280 bmp;                            // creamos el objeto bmp
-WiFiClient esp32_Client;                        // creacion de objeto wifi client
-PubSubClient client(esp32_Client);              // creacion de objeto pubsunclient
+Adafruit_BMP280 bmp;                                // creamos el objeto bmp
+WiFiClient esp32_Client;                            // creacion de objeto wifi client
+PubSubClient client(esp32_Client);                  // creacion de objeto pubsunclient
 
 void setup()
 {
-    Serial.begin(9600);                         // puerto serial nativo 9600
-    WiFi.begin(ssid, password);                 // conecto al wifi del lugar (micasa)
-    while (WiFi.status() != WL_CONNECTED)       // inicio conexion
+    Serial.begin(9600);                             // puerto serial nativo 9600
+    WiFi.begin(ssid, password);                     // conecto al wifi del lugar (micasa)
+    while (WiFi.status() != WL_CONNECTED)           // inicio conexion
     {   
-        delay(1500);                            // demora para reintentar
+        delay(1500);                                // demora para reintentar
     }
-    client.setServer(mqtt_server, mqtt_port);   // estableco conexion al server mwtt (ISPC)
-    client.setCallback(callback);               // inicio el callback de server mqtt y espero datos
-    bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);// iniciamos el objeto sensor en la direccion alterna 0x77
+    client.setServer(mqtt_server, mqtt_port);       // estableco conexion al server mwtt (ISPC)
+    client.setCallback(callback);                   // inicio el callback de server mqtt y espero datos
+    bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);   // iniciamos el objeto sensor en la direccion alterna 0x77
     /* configurar de fabrica segun datasheet ADAFRUIT Library */
     bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                     Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
@@ -35,18 +50,22 @@ void setup()
                     Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                     Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
     pinMode(LED_BUILTIN, OUTPUT);                     // el pin 2 como salida para el led azul de la placa
+    dht.begin();                                    // inicio el DHT11
+    client.setServer(mqtt_server, mqtt_port);       // estableco conexion al server mwtt (ISPC)
+    client.setCallback(callback);                   // inicio el callback de server mqtt y espero datos
+ 
 
 }
 
 void loop()
 {
-  Serial.print("Temperatura = ");          // imprimo por serial la palabra temperatura
-  Serial.print(bmp.readTemperature());     // recupero del objeto el metodo readTemperatura
-  Serial.println(" *C");                   // imprimo C de centigrado al final del renglon y hago CR/LF
-  Serial.print("Presion = ");              // imprimo por serial la palabra Presion
-  Serial.print(bmp.readPressure() / 100);  // recupero del objeto el metodo readPressure y divido para Hectopascal
-  Serial.println(" hPa");                  // imprimo Hectopascal
-  Serial.println();               // imprimo CRyLF
+//   Serial.print("Temperatura = ");          // imprimo por serial la palabra temperatura
+//   Serial.print(bmp.readTemperature());     // recupero del objeto el metodo readTemperatura
+//   Serial.println(" *C");                   // imprimo C de centigrado al final del renglon y hago CR/LF
+//   Serial.print("Presion = ");              // imprimo por serial la palabra Presion
+//   Serial.print(bmp.readPressure() / 100);  // recupero del objeto el metodo readPressure y divido para Hectopascal
+//   Serial.println(" hPa");                  // imprimo Hectopascal
+//   Serial.println();               // imprimo CRyLF
   digitalWrite(LED_BUILTIN, HIGH);// Enciendo led azul
   delay(100);
   digitalWrite(LED_BUILTIN, LOW); // apago led azul
@@ -55,9 +74,9 @@ void loop()
   delay(100);
   digitalWrite(LED_BUILTIN, LOW); // apago led azul
   delay(3000);
-      if (!client.connected()) // si la conexion esta negada reconecto
-        reconnect();
-    client.loop();
+    //   if (!client.connected()) // si la conexion esta negada reconecto
+    //     reconnect();
+    // client.loop();
 }
 
 void callback(char *topic, byte *payload, unsigned int length)
